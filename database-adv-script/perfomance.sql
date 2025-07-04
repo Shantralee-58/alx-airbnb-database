e8aefca5f9f994fa0c1a7f10f45b3fd777b55045
Shantralee-58 <lindiwekhumalo833@gmail.com>
2025-07-04 18:15:37 +0200

    4. Optimize Complex Queries

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -31,4 +31,5 @@ JOIN users u ON b.user_id = u.id
 JOIN properties p ON b.property_id = p.id
 JOIN payments pay ON b.id = pay.booking_id
 WHERE
-    b.start_date >= '2024-01-01';  -- Example filter to improve index usage
+    b.start_date >= '2024-01-01' AND
+    pay.amount > 0;
