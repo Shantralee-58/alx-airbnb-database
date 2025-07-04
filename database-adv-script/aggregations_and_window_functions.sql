a981a3ee8ab12ede71196bfd57550d047b96f48e
Shantralee-58 <lindiwekhumalo833@gmail.com>
2025-07-04 17:58:57 +0200

    2. Apply Aggregations and Window Functions Updated

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -10,3 +10,13 @@ FROM
 GROUP BY
     user_id;
 
+-- 2. Ranking properties by number of bookings
+SELECT
+    property_id,
+    COUNT(*) AS total_bookings,
+    RANK() OVER (ORDER BY COUNT(*) DESC) AS ranking,
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS row_number
+FROM
+    bookings
+GROUP BY
+    property_id;
